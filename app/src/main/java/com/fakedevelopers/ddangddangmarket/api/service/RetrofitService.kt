64d18083972718891fe6f37170dc9bf654665aba
minseonglove <qnfpdlr124@naver.com>
2022-04-02 03:41:49 +0900

    FEAT : DDM-96 AOS 로그인 화면 만들기

--- app/src/main/java/com/fakedevelopers/ddangddangmarket/api/service/RetrofitService.kt
+++ app/src/main/java/com/fakedevelopers/ddangddangmarket/api/service/RetrofitService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
-interface LoginRequestService {
+interface RetrofitService {
     @FormUrlEncoded
     @POST("user/login")
     suspend fun loginRequest(@Field("email") email: String, @Field("passwd") passwd: String): Response<String>
