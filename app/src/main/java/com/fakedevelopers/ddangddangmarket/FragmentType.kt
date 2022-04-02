ee4ad9393c58fa2c28c55dbaa0524287deaadb1f
minseonglove <qnfpdlr124@naver.com>
2022-04-02 22:49:09 +0900

    REFACTOR : konempty PR 리뷰 반영
    > * Logger 라이브러리 설치
    > * converter-scalars 라이브러리 버전업
    > * setFragment 메소드 최적화

--- app/src/main/java/com/fakedevelopers/ddangddangmarket/FragmentType.kt
+++ app/src/main/java/com/fakedevelopers/ddangddangmarket/FragmentType.kt
@@ -1,5 +1,14 @@
 package com.fakedevelopers.ddangddangmarket
 
-enum class FragmentType {
-    LOGIN, LOGINTYPE, MAIN
+import androidx.fragment.app.Fragment
+import com.fakedevelopers.ddangddangmarket.ui.login.LoginFragment
+import com.fakedevelopers.ddangddangmarket.ui.login_type.LoginTypeFragment
+import com.fakedevelopers.ddangddangmarket.ui.main.MainFragment
+
+enum class FragmentType{
+    LOGIN{ override fun get(): Fragment = LoginFragment() },
+    LOGINTYPE{ override fun get(): Fragment = LoginTypeFragment() },
+    MAIN{ override fun get(): Fragment = MainFragment() };
+
+    abstract fun get(): Fragment
 }
--- app/src/main/java/com/fakedevelopers/ddangddangmarket/MainActivity.kt
+++ app/src/main/java/com/fakedevelopers/ddangddangmarket/MainActivity.kt
@@ -2,15 +2,8 @@ package com.fakedevelopers.ddangddangmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fakedevelopers.ddangddangmarket.ui.login_type.LoginTypeFragment
-import com.fakedevelopers.ddangddangmarket.ui.login.LoginFragment
-import com.fakedevelopers.ddangddangmarket.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
-    private val loginTypeFragment = LoginTypeFragment()
-    private val loginFragment = LoginFragment()
-    private val mainFragment = MainFragment()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,19 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     fun setFragment(name : FragmentType) {
         val transition = supportFragmentManager.beginTransaction()
-        when(name){
-            FragmentType.LOGINTYPE -> {
-                transition.replace(R.id.mainContainer, loginTypeFragment)
-            }
-            FragmentType.LOGIN -> {
-                transition.replace(R.id.mainContainer, loginFragment)
-                transition.addToBackStack(null)
-            }
-            FragmentType.MAIN -> {
-                transition.replace(R.id.mainContainer, mainFragment)
-                transition.addToBackStack(null)
-            }
-        }
+        transition.replace(R.id.mainContainer, name.get())
+        if(name != FragmentType.LOGINTYPE)
+            transition.addToBackStack(null)
         transition.commit()
     }
 }
