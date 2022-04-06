0f49652f022933fd11b6ea839612458be2ae552e
minseonglove <qnfpdlr124@naver.com>
2022-04-07 00:19:58 +0900

    FIX : konempty PR 리뷰 반영
    * assertEquals메소드 패키지 네임 수정

--- app/src/androidTest/kotlin/com/fakedevelopers/bidderbidder/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/fakedevelopers/bidderbidder/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fakedevelopers.ddangddangmarket_aos", appContext.packageName)
+        assertEquals("com.fakedevelopers.bidderbidder", appContext.packageName)
     }
 }
