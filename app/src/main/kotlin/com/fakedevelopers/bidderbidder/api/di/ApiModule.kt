de14abefa5828fa49a6067f366a1b2129798c0d4
minseonglove <qnfpdlr124@naver.com>
2022-04-22 09:58:33 +0900

    STYLE : konempty 리뷰 반영

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/api/di/ApiModule.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/api/di/ApiModule.kt
@@ -15,7 +15,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.util.*
+import java.util.Locale
 import javax.inject.Singleton
 
 @Module
