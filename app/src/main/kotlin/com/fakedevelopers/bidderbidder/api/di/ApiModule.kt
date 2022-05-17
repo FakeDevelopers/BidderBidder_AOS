c09ab19bafa82ebb501f25f438b5174432442b7e
minseonglove <qnfpdlr124@naver.com>
2022-05-17 17:52:34 +0900

    CHORE : 사용하지 않는 import 제거

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/api/di/ApiModule.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/api/di/ApiModule.kt
@@ -20,7 +20,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
 import java.util.Locale
 import javax.inject.Singleton
 
