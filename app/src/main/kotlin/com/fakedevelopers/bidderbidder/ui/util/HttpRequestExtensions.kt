d9ba8c6a3cd2e39c51c60c6d7ae0933477c4b73a
re4rk <37167652+re4rk@users.noreply.github.com>
2022-08-02 19:49:12 +0900

    Update HttpRequestExtensions.kt
    FEAT : 구글 로그인 기능 구현 - 피드백 반영

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/util/HttpRequestExtensions.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/util/HttpRequestExtensions.kt
@@ -2,6 +2,6 @@ package com.fakedevelopers.bidderbidder.ui.util
 
 class HttpRequestExtensions {
     companion object {
-        const val BEARER_TOKEN_PREFIX = "Bearer"
+        const val BEARER_TOKEN_PREFIX = "Bearer "
     }
 }
