79b9134e86978cd2c6178ea1559bbb2eb98e4857
minseonglove <qnfpdlr124@naver.com>
2022-08-06 02:18:26 +0900

    REFACTOR(임시) : googleSignIn 초기화 동작을 googleLogin() 메소드 안으로 이동

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/login_type/LoginTypeFragment.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/login_type/LoginTypeFragment.kt
@@ -8,8 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -19,22 +17,15 @@ import androidx.navigation.fragment.findNavController
 import com.fakedevelopers.bidderbidder.HiltApplication
 import com.fakedevelopers.bidderbidder.R
 import com.fakedevelopers.bidderbidder.api.data.Constants.Companion.WEB_CLIENT_ID
-import com.fakedevelopers.bidderbidder.api.datastore.DatastoreSetting.Companion.BEARER_TOKEN
-import com.fakedevelopers.bidderbidder.api.datastore.DatastoreSetting.Companion.datastore
 import com.fakedevelopers.bidderbidder.databinding.FragmentLoginTypeBinding
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import java.io.IOException
 
 @AndroidEntryPoint
 class LoginTypeFragment : Fragment() {
-    private lateinit var googleSignInClient: GoogleSignInClient
     private var _binding: FragmentLoginTypeBinding? = null
 
     private val viewModel: LoginTypeViewModel by viewModels()
@@ -51,21 +42,6 @@ class LoginTypeFragment : Fragment() {
         }
     }
 
-    // datastore
-    private val dataStoreToken by lazy {
-        requireContext().datastore.data
-            .catch { exception ->
-                if (exception is IOException) {
-                    emit(emptyPreferences())
-                } else {
-                    throw exception
-                }
-            }
-            .map { preferences ->
-                preferences[BEARER_TOKEN] ?: ""
-            }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -77,11 +53,6 @@ class LoginTypeFragment : Fragment() {
             container,
             false
         )
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(WEB_CLIENT_ID)
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignInOptions)
         return binding.root
     }
 
@@ -123,9 +94,6 @@ class LoginTypeFragment : Fragment() {
                 viewModel.signinGoogleResponse.collect {
                     if (it.isSuccessful) {
                         Toast.makeText(requireActivity(), "success", Toast.LENGTH_LONG).show()
-                        requireContext().datastore.edit { preferences ->
-                            preferences[BEARER_TOKEN] = viewModel.token.value
-                        }
                         findNavController().navigate(R.id.action_loginTypeFragment_to_productListFragment)
                     } else {
                         Toast.makeText(requireActivity(), "failure", Toast.LENGTH_LONG).show()
@@ -141,6 +109,10 @@ class LoginTypeFragment : Fragment() {
     }
 
     private fun googleLogin() {
-        requestActivity.launch(googleSignInClient.signInIntent)
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(WEB_CLIENT_ID)
+            .requestEmail()
+            .build()
+        requestActivity.launch(GoogleSignIn.getClient(requireActivity(), googleSignInOptions).signInIntent)
     }
 }
