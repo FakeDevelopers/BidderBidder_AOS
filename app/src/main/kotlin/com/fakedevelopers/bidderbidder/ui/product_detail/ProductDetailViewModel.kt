4d117912bf0dc96d99cad06b429fde1f4cce2097
minseonglove <qnfpdlr124@naver.com>
2022-08-23 14:59:51 +0900

    FEAT : ProductDetailRepository 연결

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/product_detail/ProductDetailViewModel.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/product_detail/ProductDetailViewModel.kt
@@ -1,12 +1,27 @@
 package com.fakedevelopers.bidderbidder.ui.product_detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.fakedevelopers.bidderbidder.api.repository.ProductDetailRepository
+import com.fakedevelopers.bidderbidder.ui.util.MutableEventFlow
+import com.fakedevelopers.bidderbidder.ui.util.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductDetailViewModel @Inject constructor(
     private val repository: ProductDetailRepository
 ) : ViewModel() {
+
+    private val _productDetailResponse = MutableEventFlow<Response<ProductDetailDto>>()
+
+    private val productDetailResponse = _productDetailResponse.asEventFlow()
+
+    private fun productDetailRequest(productId: Long) {
+        viewModelScope.launch {
+            _productDetailResponse.emit(repository.getProductDetail(productId))
+        }
+    }
 }
