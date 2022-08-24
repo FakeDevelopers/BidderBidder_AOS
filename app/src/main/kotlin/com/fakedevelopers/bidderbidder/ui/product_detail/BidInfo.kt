9b545839d5d52ba1286b714b9e132e0d07caa05e
minseonglove <qnfpdlr124@naver.com>
2022-08-24 17:19:02 +0900

    FEAT : ProductDetailDto 스팩 변경
    * 입찰 정보 리스트를 받는 bids를 추가했습니다.

---
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/product_detail/BidInfo.kt
@@ -0,0 +1,7 @@
+package com.fakedevelopers.bidderbidder.ui.product_detail
+
+data class BidInfo(
+    val bid: Long,
+    val index: Int,
+    val userNickName: String
+)
--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/product_detail/ProductDetailDto.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/ui/product_detail/ProductDetailDto.kt
@@ -2,14 +2,15 @@ package com.fakedevelopers.bidderbidder.ui.product_detail
 
 data class ProductDetailDto(
     val bidderCount: Int,
+    val bids: List<BidInfo>,
     val createdTime: String,
     val expirationDate: String,
-    val hopePrice: Int,
+    val hopePrice: Long,
     val images: List<String>,
-    val openingBid: Int,
+    val openingBid: Long,
     val productContent: String,
     val productTitle: String,
-    val tick: Int
+    val tick: Long
 ) {
     override fun toString(): String {
         val sb = StringBuilder()
