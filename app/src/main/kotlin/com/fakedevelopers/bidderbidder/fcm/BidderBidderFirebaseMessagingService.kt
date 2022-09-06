5598d49439cee5163658e534ebd518639e9be728
minseonglove <qnfpdlr124@naver.com>
2022-09-07 01:18:28 +0900

    FEAT : firebase 메세지를 받으면 notification을 띄우는 기능 추가

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/fcm/BidderBidderFirebaseMessagingService.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/fcm/BidderBidderFirebaseMessagingService.kt
@@ -1,15 +1,69 @@
 package com.fakedevelopers.bidderbidder.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.fakedevelopers.bidderbidder.MainActivity
+import com.fakedevelopers.bidderbidder.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class BidderBidderFirebaseMessagingService : FirebaseMessagingService() {
 
+    private val notificationManager by lazy {
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        message.notification?.let { notification ->
+            val title = notification.title ?: ""
+            val body = notification.body ?: ""
+            sendNotification(title, body)
+        }
+    }
+
+    private fun sendNotification(title: String, body: String) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        val largeIcon = BitmapFactory.decodeResource(resources, R.drawable.seller_cat)
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setLargeIcon(largeIcon)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply {
+            description = "FCM 메세지 전송"
+            enableLights(true)
+            enableVibration(true)
+            lightColor = Color.GREEN
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
