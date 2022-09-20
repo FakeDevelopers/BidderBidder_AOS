fb7b8ed29162f8e513212557522bc74afe696024
minseonglove <qnfpdlr124@naver.com>
2022-09-20 22:33:10 +0900

    REFACTOR : 입찰 api 파라미터 어노테이션을 다시 Field로 변경

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/api/service/ProductDetailService.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/api/service/ProductDetailService.kt
@@ -2,10 +2,11 @@ package com.fakedevelopers.bidderbidder.api.service
 
 import com.fakedevelopers.bidderbidder.ui.product_detail.ProductDetailDto
 import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface ProductDetailService {
     @GET("product/getProductInfo/{productId}")
@@ -13,10 +14,11 @@ interface ProductDetailService {
         @Path("productId") productId: Long
     ): Response<ProductDetailDto>
 
+    @FormUrlEncoded
     @POST("product/{productId}/bid")
     suspend fun postProductBid(
         @Path("productId") productId: Long,
-        @Query("userId") userId: Long,
-        @Query("bid") bid: Long
+        @Field("userId") userId: Long,
+        @Field("bid") bid: Long
     ): Response<String>
 }
