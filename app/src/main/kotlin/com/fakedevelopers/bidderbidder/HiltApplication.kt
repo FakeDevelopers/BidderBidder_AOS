55b3c549596f57a064f5a1778e39d0fce14068f7
minseonglove <qnfpdlr124@naver.com>
2022-09-23 21:44:08 +0900

    CHORE : 테스트 코드 제거

--- app/src/main/kotlin/com/fakedevelopers/bidderbidder/HiltApplication.kt
+++ app/src/main/kotlin/com/fakedevelopers/bidderbidder/HiltApplication.kt
@@ -8,7 +8,6 @@ import com.fakedevelopers.bidderbidder.api.data.Constants.Companion.SENTRY_DSN
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
-import io.sentry.Sentry
 import io.sentry.android.core.SentryAndroid
 
 @HiltAndroidApp
@@ -18,11 +17,6 @@ class HiltApplication : Application() {
         super.onCreate()
         Logger.addLogAdapter(AndroidLogAdapter())
         initSentry()
-        try {
-            throw Exception("야옹")
-        } catch (e: Exception) {
-            Sentry.captureException(e)
-        }
     }
 
     private fun initSentry() {
