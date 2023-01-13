06687d1ab6dac2cd30e4b0774a09bf1e1af30277
minseonglove <qnfpdlr124@naver.com>
2023-01-13 19:25:48 +0900

    CHORE : 잘가 BDBDResult

--- domain/src/main/java/com/minseonglove/domain/model/BDBDResult.kt
+++
@@ -1,6 +0,0 @@
-package com.minseonglove.domain.model
-
-sealed class BDBDResult<out T : Any> {
-    data class Success<out T : Any>(val data: T) : BDBDResult<T>()
-    data class Error(val e: Exception) : BDBDResult<Nothing>()
-}
