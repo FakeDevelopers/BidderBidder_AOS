fb0054e4ff1e49df0bfd22dc9729ec20aa883633
minseonglove <qnfpdlr124@naver.com>
2023-01-17 23:57:44 +0900

    FEAT : Data 모듈 AuthRepository 추가

---
+++ data/src/main/java/com/fakedevelopers/data/util/AuthInterceptor.kt
@@ -0,0 +1,20 @@
+package com.fakedevelopers.data.util
+
+import com.fakedevelopers.domain.model.BEARER_TOKEN_PREFIX
+import com.google.firebase.auth.FirebaseAuth
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val firebase: FirebaseAuth
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+        val newRequest = request().newBuilder()
+        firebase.currentUser?.getIdToken(false)?.let {
+            val authorization = BEARER_TOKEN_PREFIX + it.result.token
+            newRequest.addHeader("Authorization", authorization)
+        }
+        proceed(newRequest.build())
+    }
+}
