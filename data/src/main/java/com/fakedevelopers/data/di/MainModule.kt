952363177b6893dcaa62ef09fdc7b800ff93442f
minseonglove <qnfpdlr124@naver.com>
2023-02-01 16:24:21 +0900

    CHORE : 빠진 임포트 추가

--- data/src/main/java/com/fakedevelopers/data/di/MainModule.kt
+++ data/src/main/java/com/fakedevelopers/data/di/MainModule.kt
@@ -2,11 +2,14 @@ package com.fakedevelopers.data.di
 
 import android.content.Context
 import com.fakedevelopers.data.repository.ImageRepositoryImpl
+import com.fakedevelopers.data.repository.LocalStorageRepositoryImpl
 import com.fakedevelopers.data.repository.ProductEditorRepositoryImpl
 import com.fakedevelopers.data.repository.ProductListRepositoryImpl
 import com.fakedevelopers.data.service.ProductEditorService
 import com.fakedevelopers.data.service.ProductListService
+import com.fakedevelopers.data.source.LocalStorageDataSource
 import com.fakedevelopers.domain.repository.ImageRepository
+import com.fakedevelopers.domain.repository.LocalStorageRepository
 import com.fakedevelopers.domain.repository.ProductEditorRepository
 import com.fakedevelopers.domain.repository.ProductListRepository
 import dagger.Module
